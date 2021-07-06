67b87c658f0cfa853139be6e0abde991e1645f9e
fabiola29298 <fabiola29298@gmail.com>
2021-07-06 18:56:50 -0400

    added form and input

--- src/index.js
+++ src/index.js
@@ -6,3 +6,13 @@ import './styles.css';
 // autoComplete();
 // mostPopularMovies();
 // topCrew();
+
+document.querySelector('#bar-toggle').addEventListener('click', async () => {
+  document.querySelector('.header').style.visibility = "hidden";
+  document.querySelector('.header-search').style.visibility = "visible";
+});
+
+document.querySelector('#bar-back').addEventListener('click', async () => {
+  document.querySelector('.header').style.visibility = "visible";
+  document.querySelector('.header-search').style.visibility = "hidden";
+});
